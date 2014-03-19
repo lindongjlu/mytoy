e01a1bee690b84b0bbd0b85c1841c5ad88e553d1
lindongjlu <lindongjlu@gmail.com>
2014-03-19 15:35:31 +0800

    a

--- TestNetty/src/main/java/netty/test/TestClient.java
+++ TestNetty/src/main/java/netty/test/TestClient.java
@@ -1,77 +1,80 @@
 package netty.test;
 
-import io.netty.bootstrap.Bootstrap;
+import java.net.InetSocketAddress;
+
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class TestClient {
 
-	private final String host = "127.0.0.1";
-    private final int port = 1237;
+	private final String host;
+	private final int port;
 
-    public TestClient() {
-    }
+	public TestClient(String host, int port) {
+		this.host = host;
+		this.port = port;
+	}
 
-    public void run() throws Exception {
-        // Configure the client.
-        EventLoopGroup group = new NioEventLoopGroup();
-        try {
-            Bootstrap b = new Bootstrap();
-            b.group(group)
-             .channel(NioSocketChannel.class)
-             .option(ChannelOption.TCP_NODELAY, true)
-             .handler(new EmptyHandler());
+	public void run() throws Exception {
+		EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
 
-            // Start the client.
-            ChannelFuture f = b.connect(host, port).sync();
+		NioSocketChannel nioSocketChannel = new NioSocketChannel();
+		nioSocketChannel.config().setTcpNoDelay(true);
+		// nioSocketChannel.pipeline().addLast(new EmptyHandler());
 
-            f.channel().writeAndFlush(Unpooled.wrappedBuffer("你好".getBytes())).sync();
-            
-            // Wait until the connection is closed.
-            f.channel().closeFuture().sync();
-        } finally {
-            // Shut down the event loop to terminate all threads.
-            group.shutdownGracefully();
-        }
-    }
+		eventLoopGroup.register(nioSocketChannel).get();
+		System.out.println("channel register!");
 
-    public static void main(String[] args) throws Exception {
-        new TestClient().run();
-    }
+		nioSocketChannel.connect(new InetSocketAddress(host, port)).get();
+		System.out.println("channel connect!");
+
+		System.out.println("channel isActive:" + nioSocketChannel.isActive());
+
+		nioSocketChannel.writeAndFlush(Unpooled.wrappedBuffer("你好".getBytes()))
+				.get();
+
+		nioSocketChannel.close().get();
+		System.out.println("channel close!");
+
+		eventLoopGroup.shutdownGracefully();
+	}
+
+	public static void main(String[] args) throws Exception {
+		new TestClient("127.0.0.1", 1237).run();
+	}
 }
 
 class EmptyHandler extends ChannelInboundHandlerAdapter {
 
-    /**
-     * Creates a client-side handler.
-     */
-    public EmptyHandler() {
-    }
+	/**
+	 * Creates a client-side handler.
+	 */
+	public EmptyHandler() {
+	}
 
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) {
-    }
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) {
+	}
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        // ctx.write(msg);
-    	System.out.println(msg);
-    }
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		// ctx.write(msg);
+		System.out.println(msg);
+	}
 
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-       ctx.flush();
-    }
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		ctx.flush();
+	}
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        cause.printStackTrace();
-        ctx.close();
-    }
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
 }
--- TestNetty/src/main/java/netty/test/TestServer.java
+++ TestNetty/src/main/java/netty/test/TestServer.java
@@ -1,7 +1,15 @@
 package netty.test;
 
-import io.netty.bootstrap.ServerBootstrap;
+import java.net.InetSocketAddress;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Map.Entry;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
@@ -11,70 +19,149 @@ import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.logging.LogLevel;
-import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.AttributeKey;
 
 public class TestServer {
 
-    private final int port = 1237;
+	private final int port = 1237;
 
-    public TestServer() { 
-    }
+	public TestServer() {
+	}
 
-    public void run() throws Exception {
-        // Configure the server.
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-        try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-             .channel(NioServerSocketChannel.class)
-             .option(ChannelOption.SO_BACKLOG, 100)
-             .handler(new LoggingHandler(LogLevel.INFO))
-             .childHandler(new ChannelInitializer<SocketChannel>() {
-                 @Override
-                 public void initChannel(SocketChannel ch) throws Exception {
-                     ch.pipeline().addLast(
-                             //new LoggingHandler(LogLevel.INFO),
-                             new EchoServerHandler());
-                 }
-             });
+	public void run() throws Exception {
+		// Configure the server.
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
 
-            // Start the server.
-            ChannelFuture f = b.bind(port).sync();
+		NioServerSocketChannel nioServerSocketChannel = new NioServerSocketChannel();
+		nioServerSocketChannel.config().setBacklog(100);
+		nioServerSocketChannel.pipeline().addLast(
+				new Acceptor(workerGroup,
+						new ChannelInitializer<SocketChannel>() {
 
-            // Wait until the server socket is closed.
-            f.channel().closeFuture().sync();
-        } finally {
-            // Shut down all event loops to terminate all threads.
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
-        }
-    }
+							@Override
+							protected void initChannel(SocketChannel ch)
+									throws Exception {
+								ch.pipeline().addLast(new EchoServerHandler());
+							}
 
-    public static void main(String[] args) throws Exception {
-        new TestServer().run();
-    }
+						}));
 
+		bossGroup.register(nioServerSocketChannel).get();
+		System.out.println("server channel register!");
+
+		nioServerSocketChannel.bind(new InetSocketAddress("127.0.0.1", port))
+				.get();
+		System.out.println("server channel bind!");
+
+		nioServerSocketChannel.closeFuture().sync();
+
+		bossGroup.shutdownGracefully();
+		workerGroup.shutdownGracefully();
+	}
+
+	public static void main(String[] args) throws Exception {
+		new TestServer().run();
+	}
+
+}
+
+class Acceptor extends ChannelInboundHandlerAdapter {
+
+	private final EventLoopGroup childGroup;
+	private final ChannelHandler childHandler;
+	private final Map<ChannelOption<?>, Object> childOptions;
+	private final Map<AttributeKey<?>, Object> childAttrs;
+
+	Acceptor(EventLoopGroup childGroup, ChannelHandler childHandler,
+			Map<ChannelOption<?>, Object> childOptions,
+			Map<AttributeKey<?>, Object> childAttrs) {
+		this.childGroup = childGroup;
+		this.childHandler = childHandler;
+		this.childOptions = childOptions;
+		this.childAttrs = childAttrs;
+	}
+
+	Acceptor(EventLoopGroup childGroup, ChannelHandler childHandler) {
+		this.childGroup = childGroup;
+		this.childHandler = childHandler;
+		this.childOptions = Collections.emptyMap();
+		this.childAttrs = Collections.emptyMap();
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		final Channel child = (Channel) msg;
+
+		child.pipeline().addLast(childHandler);
+
+		for (Entry<ChannelOption<?>, Object> e : childOptions.entrySet()) {
+			try {
+				if (!child.config().setOption(
+						(ChannelOption<Object>) e.getKey(), e.getValue())) {
+					System.err.println("Unknown channel option: " + e);
+				}
+			} catch (Throwable t) {
+				System.err.println("Failed to set a channel option: " + child);
+				t.printStackTrace();
+			}
+		}
+
+		for (Entry<AttributeKey<?>, Object> e : childAttrs.entrySet()) {
+			child.attr((AttributeKey<Object>) e.getKey()).set(e.getValue());
+		}
+
+		try {
+			childGroup.register(child).addListener(new ChannelFutureListener() {
+				@Override
+				public void operationComplete(ChannelFuture future)
+						throws Exception {
+					if (!future.isSuccess()) {
+						forceClose(child, future.cause());
+					}
+				}
+			});
+		} catch (Throwable t) {
+			forceClose(child, t);
+		}
+	}
+
+	private static void forceClose(Channel child, Throwable t) {
+		child.unsafe().closeForcibly();
+		System.err.println("Failed to register an accepted channel: " + child);
+		t.printStackTrace();
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+			throws Exception {
+		ctx.fireExceptionCaught(cause);
+	}
 }
 
 @Sharable
 class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ctx.write(msg);
-    }
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		ByteBuf byteBuf = (ByteBuf) msg;
+		byte[] bytes = new byte[byteBuf.readableBytes()];
+		byteBuf.readBytes(bytes);
+		System.out.println("recive: " + new String(bytes));
+		ctx.write(Unpooled.wrappedBuffer(bytes));
+	}
 
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        ctx.flush();
-    }
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		ctx.flush();
+	}
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        // Close the connection when an exception is raised.
-    	cause.printStackTrace();
-        ctx.close();
-    }
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		// Close the connection when an exception is raised.
+		cause.printStackTrace();
+		ctx.close();
+	}
 }
