6fd6c5173f984ab1cb912e1b0cbf1f1db6fa4c4d
lindongjlu <lindongjlu@gmail.com>
2014-05-07 19:11:44 +0800

    1

--- TestThrift/src/main/java/lindongjlu/thrift/TAbstractClient.java
+++ TestThrift/src/main/java/lindongjlu/thrift/TAbstractClient.java
@@ -6,41 +6,37 @@ import com.google.common.base.Function;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 
-public abstract class TAbstractClient<T extends TAbstractClient<?>> implements TBaseClient<T> {
+public abstract class TAbstractClient<T extends TAbstractClient<?>> implements
+		TBaseClient<T> {
 
 	private final TBaseClient<?> baseClient;
-	
+
 	protected TAbstractClient(TBaseClient<?> baseClient) {
 		this.baseClient = baseClient;
 	}
-	
+
 	@Override
 	public boolean isOpen() {
 		return baseClient.isOpen();
 	}
 
+	private final Function<TBaseClient<?>, T> transformFunc = new Function<TBaseClient<?>, T>() {
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public T apply(TBaseClient<?> input) {
+			return (T) TAbstractClient.this;
+		}
+	};
+
 	@Override
 	public ListenableFuture<T> open() {
-		return Futures.transform(baseClient.open(), new Function<TBaseClient<?>, T>() {
-
-			@SuppressWarnings("unchecked")
-			@Override
-			public T apply(TBaseClient<?> input) {
-				return (T) TAbstractClient.this;
-			}
-		});
+		return Futures.transform(baseClient.open(), transformFunc);
 	}
 
 	@Override
 	public ListenableFuture<T> close() {
-		return Futures.transform(baseClient.close(), new Function<TBaseClient<?>, T>() {
-
-			@SuppressWarnings("unchecked")
-			@Override
-			public T apply(TBaseClient<?> input) {
-				return (T) TAbstractClient.this;
-			}
-		});
+		return Futures.transform(baseClient.close(), transformFunc);
 	}
 
 	@Override
@@ -48,5 +44,5 @@ public abstract class TAbstractClient<T extends TAbstractClient<?>> implements T
 			String methodName, TBase<?, ?> args, R result) {
 		return baseClient.callBase(methodName, args, result);
 	}
-	
+
 }
