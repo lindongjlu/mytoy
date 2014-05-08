1a6d10c041107e77d8526bd783bbd9d51520a08c
lindongjlu <lindongjlu@gmail.com>
2014-05-08 10:56:15 +0800

    remove

--- TestThrift/src/main/java/thrift/test/TestClientEx.java
+++ TestThrift/src/main/java/thrift/test/TestClientEx.java
@@ -1,21 +1,11 @@
 package thrift.test;
 
-import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioSocketChannel;
-
-import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.util.concurrent.ExecutionException;
-
 import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.protocol.TProtocolFactory;
-import org.apache.thrift.transport.TTransport;
-
 import com.google.common.util.concurrent.ListenableFuture;
 
-import lindongjlu.thrift.TNettyHandle;
 import lindongjlu.thrift.TNettyNioSocketClient;
 
 public class TestClientEx {
@@ -27,20 +17,6 @@ public class TestClientEx {
 	public void startClient() throws Throwable {
 		
 		// netty init start
-		
-//		TNettyHandle nettyHandle = new TNettyHandle(new TBinaryProtocol.Factory());
-//		
-//		EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
-//
-//		NioSocketChannel nioSocketChannel = new NioSocketChannel();
-//		nioSocketChannel.config().setTcpNoDelay(true);
-//		nioSocketChannel.pipeline().addLast(nettyHandle);
-//
-//		eventLoopGroup.register(nioSocketChannel).get();
-//		System.out.println("channel register!");
-//
-//		nioSocketChannel.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT)).get();
-//		System.out.println("channel connect!");
 		
 		NioEventLoopGroup eventLoopGroup = new NioEventLoopGroup();
 		
@@ -55,9 +31,10 @@ public class TestClientEx {
 		
 		TestMessage testMsg = new TestMessage("topic", ByteBuffer.wrap("123".getBytes()), 2, "哇咔咔", 123);
 		
-		ListenableFuture<Long> f = client.ping(279, testMsg);
-		System.out.println("Thrify client result =: " + f.get());
-		
+		for (int i = 123; i<223; ++i) {
+			ListenableFuture<Long> f = client.ping(i, testMsg);
+			System.out.println("Thrify client result =: " + f.get());
+		}
 		client.close().get();
 		eventLoopGroup.shutdownGracefully().get();
 	}
